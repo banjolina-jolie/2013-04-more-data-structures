5232f42e28ab2f12cd77c6df9338e4c1e5ce8005
Dylan O'Carroll <dylanocarroll@MyLaptop.local>
2013-05-01 22:18:40 -0700

    added value to initial tree

--- src/tree.js
+++ src/tree.js
@@ -6,24 +6,27 @@ var makeTree = function(val){
   return newTree;
 };
 
+
 var treeMethods = {};
+
 
 treeMethods.addChild = function(val){
   this.children.push(makeTree(val));
 };
+
 
 treeMethods.contains = function(val){
   if(this.value === val) {
     return true;
   }
   for(var i = 0; i < this.children.length; i++) {
-    var refer = this.children[i];
     if(this.children[i].contains(val)) {
       return true;
     }
   }
   return false;
 };
+
 
 var extend = function(to, from){
   for (var key in from){
